ca7a6ebc5861d7482c4fc20610df5d735113a510
rlawlsyoung <jinyoung01099@gmail.com>
2023-05-26 20:12:40 +0900

    [Add] 동물 이름짓기 form 생성

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,23 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
-  return <>ddd</>;
+  return (
+    <>
+      <div>
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main>
+          <img src="/favicon.ico" alt="favicon" />
+          <h3>애완동물 이름 짓기</h3>
+          <form>
+            <input type="text" name="animal" placeholder="Enter an animal" />
+            <input type="submit" />
+          </form>
+        </main>
+      </div>
+    </>
+  );
 }
